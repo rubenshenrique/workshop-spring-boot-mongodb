1dec5a6d7f73bdede0853819a76231d46ff90aee
Rubens Henrique <rubens.autovaz@gmail.com>
2023-10-31 20:23:02 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/rubenshenrique/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenshenrique.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
