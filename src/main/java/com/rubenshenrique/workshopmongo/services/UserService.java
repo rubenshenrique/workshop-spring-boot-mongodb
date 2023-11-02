79d57f4f4af50e430e333099be77126fef98231e
Rubens Henrique <rubens.autovaz@gmail.com>
2023-11-02 18:11:06 -0300

    Inserção de usuário com POST

--- src/main/java/com/rubenshenrique/workshopmongo/services/UserService.java
+++ src/main/java/com/rubenshenrique/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rubenshenrique.workshopmongo.domain.User;
+import com.rubenshenrique.workshopmongo.dto.UserDTO;
 import com.rubenshenrique.workshopmongo.repository.UserRepository;
 import com.rubenshenrique.workshopmongo.services.exception.ObjectNotFoundExcepetion;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundExcepetion("Objeto Não Encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 
 }
