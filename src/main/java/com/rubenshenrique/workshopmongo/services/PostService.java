41b4c1b2483461157c86cbc1b291cf2bb7395a32
Rubens Henrique <rubens.autovaz@gmail.com>
2023-11-06 22:59:50 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rubenshenrique/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.rubenshenrique.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rubenshenrique.workshopmongo.domain.Post;
+import com.rubenshenrique.workshopmongo.domain.User;
+import com.rubenshenrique.workshopmongo.repository.PostRepository;
+import com.rubenshenrique.workshopmongo.services.exception.ObjectNotFoundExcepetion;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExcepetion("Objeto Não Encontrado"));
+		}
+}
